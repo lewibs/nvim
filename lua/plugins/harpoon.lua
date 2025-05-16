6ad5e51d18a849eaddd367733728d2cbff230f74
blewis <blewis@smarttech.com>
2025-05-16 13:28:20 -0400

    updated:q Please enter the commit message for your changes. Lines starting

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -11,7 +11,7 @@ return {
       { "<leader>r", "<cmd>lua require('harpoon.mark').rm_file()<cr>", desc = "Remove file from Harpoon" },
       { "<leader>m", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Toggle Harpoon menu" },
       { "<C-n>", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Next file in Harpoon" },
-      { "<C-p>", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Previous file in Harpoon" },
+      { "<C-m>", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Previous file in Harpoon" },
       -- { "<C-1>", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "Go to file 1" },
       -- { "<C-2>", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "Go to file 2" },
       -- { "<C-3>", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "Go to file 3" },
