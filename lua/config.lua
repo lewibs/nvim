1d6dae3a3f6de14f9ab9958446b0e4143a5541df
blewis <blewis@smarttech.com>
2025-05-16 13:33:50 -0400

    remved smart indenting, it doesnt work rn

--- lua/config.lua
+++ lua/config.lua
@@ -8,8 +8,8 @@ vim.opt.splitright = true         -- New vertical splits go to the right (style)
 vim.opt.expandtab = true          -- Convert tabs to spaces (style)
 vim.opt.tabstop = 5               -- Number of spaces per tab (style)
 vim.opt.shiftwidth = 1            -- Use tabstop value for autoindent (style)
-vim.opt.smartindent = true
-vim.opt.autoindent = true
+--vim.opt.smartindent = true
+--vim.opt.autoindent = true
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
 vim.g.mapleader = " "
 
