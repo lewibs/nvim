6b16f32ba54d015f5eda5eb74e8b4a0fa6a3b147
Your Name <you@example.com>
2025-05-16 00:38:24 -0400

    added gf git-files to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,8 @@
+if vim.fn.executable("rg") == 0 then
+  -- If ripgrep is not installed, show a warning
+  vim.notify("Ripgrep (rg) is not installed. Live Grep won't work.", vim.log.levels.WARN)
+end
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -17,8 +22,9 @@ return {
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
       { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
-      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
-      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find Help" },
+      { "<leader>gf", "<cmd>Telescope git_files<cr>", desc = "Search Git" },
+      --{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
+      --{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Find Help" },
     },
   },
 }
