d5e242df01def28de2351fbe9373e7c66eb6f9c9
blewis <blewis@smarttech.com>
2025-05-16 13:55:15 -0400

    fixed keymapps that overwrite

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,34 @@ return {
   {
     "neovim/nvim-lspconfig",
     lazy = true,
+    keys = {
+      -- Show error under cursor with <leader>e
+      {
+        "<leader>e",
+        function()
+          vim.diagnostic.open_float()  -- Shows error under cursor in a floating window
+        end,
+        desc = "Show error under cursor",
+      },
+      
+      -- Next diagnostic with Ctrl-n
+      {
+        "<C-h>",
+        function()
+          vim.diagnostic.goto_next()  -- Go to next diagnostic/error
+        end, 
+        desc = "Next diagnostic",
+      },
+      
+      -- Previous diagnostic with Ctrl-m
+      {
+        "<C-j>",
+        function()
+          vim.diagnostic.goto_prev()  -- Go to previous diagnostic/error
+        end,
+        desc = "Previous diagnostic",
+      },
+    },
   },
 
   -- Autocompletion source for LSP
