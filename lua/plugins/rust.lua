40c0f1a50e5a185b053623e83d0a36f5388fc0c0
Your Name <you@example.com>
2025-05-19 10:40:23 -0400

    rust

---
+++ lua/plugins/rust.lua
@@ -0,0 +1,51 @@
+return {
+  -- Rust LSP via lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ft = { "rust" },
+    config = function()
+      require("lspconfig").rust_analyzer.setup({
+        settings = {
+          ["rust-analyzer"] = {
+            check = {
+              command = "clippy",
+            },
+            diagnostics = {
+              enable = true,
+            },
+          },
+        },
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      })
+    end,
+  },
+
+  -- Completion engine
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter", "CmdlineEnter" },
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-vsnip",
+      "hrsh7th/vim-vsnip",
+    },
+    config = function()
+      require("cmp").setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "vsnip" },
+          { name = "path" },
+          { name = "buffer" },
+        },
+      })
+    end,
+  },
+}
+
