2a3ac35874daa5b837d381c3056245f10e53974a
blewis <blewis@smarttech.com>
2025-05-22 13:28:38 -0400

    added keys

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -10,7 +10,7 @@ return {
           callback({
             type = "server",
             host = "127.0.0.1",
-            port = 5005  -- standard Java debug port
+            port = 5005,
           })
         end
 
@@ -20,11 +20,19 @@ return {
             request = "attach",
             name = "Attach to Java",
             hostName = "127.0.0.1",
-            port = 5005
+            port = 5005,
           }
         }
-      end
-    }
-  }
+
+        -- Ctrl+c based DAP keybindings
+        vim.keymap.set("n", "<C-c>", dap.continue, { desc = "DAP: Continue" })
+        vim.keymap.set("n", "<C-o>", dap.step_over, { desc = "DAP: Step Over" })
+        vim.keymap.set("n", "<C-i>", dap.step_into, { desc = "DAP: Step Into" })
+        vim.keymap.set("n", "<C-u>", dap.step_out, { desc = "DAP: Step Out" })
+        vim.keymap.set("n", "<C-b>", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
+        --vim.keymap.set("n", "<C-r>", dap.repl.open, { desc = "DAP: Open REPL" })
+      end,
+    },
+  },
 }
 
