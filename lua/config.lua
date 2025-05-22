66c38ca36eaff526c4623164fe2016244db74818
blewis <blewis@smarttech.com>
2025-05-22 10:45:34 -0400

    Working on indents

--- lua/config.lua
+++ lua/config.lua
@@ -5,11 +5,14 @@ vim.opt.number = true             -- Show line numbers (style)
 vim.opt.relativenumber = true     -- Show relative line numbers (style)
 vim.opt.splitbelow = true         -- New horizontal splits go below (style)
 vim.opt.splitright = true         -- New vertical splits go to the right (style)
+
 vim.opt.expandtab = true          -- Convert tabs to spaces (style)
---vim.opt.tabstop = 5               -- Number of spaces per tab (style)
---vim.opt.shiftwidth = 1            -- Use tabstop value for autoindent (style)
---vim.opt.smartindent = true
---vim.opt.autoindent = true
+vim.opt.tabstop = 4           -- A tab counts as 4 spaces (when reading)
+vim.opt.shiftwidth = 4        -- Indent by 4 spaces
+vim.opt.softtabstop = 4       -- Number of spaces a <Tab> counts for in insert mode
+vim.opt.smartindent = true    -- Smart autoindenting on new lines
+vim.opt.autoindent = true     -- Copy indent from current line when starting a new line
+
 vim.opt.clipboard = "unnamedplus" -- use system clipboard
 vim.g.mapleader = " "
 
