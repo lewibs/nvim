3955cfedee463b4405fdb0dd1e3e26131b12c907
blewis <blewis@smarttech.com>
2025-05-22 13:34:07 -0400

    udpated maps

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,38 +1,35 @@
+
 return {
   "mfussenegger/nvim-dap",
-  dependencies = {
-    {
-      "mfussenegger/nvim-dap",
-      config = function()
-        local dap = require("dap")
+  config = function()
+    local dap = require("dap")
 
-        dap.adapters.java = function(callback)
-          callback({
-            type = "server",
-            host = "127.0.0.1",
-            port = 5005,
-          })
-        end
+    -- Java DAP adapter (attach)
+    dap.adapters.java = function(callback)
+      callback({
+        type = "server",
+        host = "127.0.0.1",
+        port = 5005,
+      })
+    end
 
-        dap.configurations.java = {
-          {
-            type = "java",
-            request = "attach",
-            name = "Attach to Java",
-            hostName = "127.0.0.1",
-            port = 5005,
-          }
-        }
+    dap.configurations.java = {
+      {
+        type = "java",
+        request = "attach",
+        name = "Attach to Java",
+        hostName = "127.0.0.1",
+        port = 5005,
+      }
+    }
 
-        -- Ctrl+c based DAP keybindings
-        vim.keymap.set("n", "<C-c>", dap.continue, { desc = "DAP: Continue" })
-        vim.keymap.set("n", "<C-o>", dap.step_over, { desc = "DAP: Step Over" })
-        vim.keymap.set("n", "<C-i>", dap.step_into, { desc = "DAP: Step Into" })
-        vim.keymap.set("n", "<C-u>", dap.step_out, { desc = "DAP: Step Out" })
-        vim.keymap.set("n", "<C-b>", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
-        --vim.keymap.set("n", "<C-r>", dap.repl.open, { desc = "DAP: Open REPL" })
-      end,
-    },
-  },
+    -- Common leader-based DAP keybindings
+    vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "DAP: Continue" })
+    vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "DAP: Step Over" })
+    vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "DAP: Step Into" })
+    vim.keymap.set("n", "<leader>du", dap.step_out, { desc = "DAP: Step Out" })
+    vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
+    vim.keymap.set("n", "<leader>dr", dap.repl.open, { desc = "DAP: Open REPL" })
+  end,
 }
 
