4e7a407f86141ef50c1042b95fb847c1dac3cd8b
Your Name <you@example.com>
2025-11-02 16:43:40 -0500

    added supermaven

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -14,7 +14,7 @@ return {
       vim.keymap.set("n", "<leader>r", function() require("harpoon.mark").rm_file() end, { desc = "Remove file from Harpoon" })
       vim.keymap.set("n", "<leader>m", function() require("harpoon.ui").toggle_quick_menu() end, { desc = "Toggle Harpoon menu" })
       vim.keymap.set("n", "<C-n>", function() require("harpoon.ui").nav_next() end, { desc = "Next file in Harpoon" })
-      -- vim.keymap.set("n", "<C-m>", function() require("harpoon.ui").nav_prev() end, { desc = "Previous file in Harpoon" })
+      -- vim.keymap.set("n", "<C-m>", function() require("harpoon.ui").nav_prev() end, { desc = "Previous fil in Harpoon" })
       -- vim.keymap.set("n", "<C-1>", function() require("harpoon.ui").nav_file(1) end, { desc = "Go to file 1" })
       -- vim.keymap.set("n", "<C-2>", function() require("harpoon.ui").nav_file(2) end, { desc = "Go to file 2" })
       -- vim.keymap.set("n", "<C-3>", function() require("harpoon.ui").nav_file(3) end, { desc = "Go to file 3" })
