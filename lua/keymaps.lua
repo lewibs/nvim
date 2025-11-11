ab5063539b36ab8653eea82f771b934d282dc4a2
Your Name <you@example.com>
2025-11-11 00:00:21 -0500

    added chatgpt command

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,9 @@
 vim.keymap.set("n", "<CR>", "i", { noremap = true, silent = true })
+
 vim.keymap.set("n", "<leader>t", function()
   vim.cmd("split | terminal cd " .. vim.fn.expand("%:p:h") .. " && $SHELL")
+end)
+
+vim.keymap.set("n", "<leader>c", function()
+    vim.cmd("vsp | terminal codex")
 end)
