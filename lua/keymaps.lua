8847318147f21d411afc3c2c8d682bf9e361804a
Your Name <you@example.com>
2025-11-11 00:13:23 -0500

    fixed gpt keymaps"

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,4 +4,10 @@ vim.keymap.set("n", "<leader>t", function()
   vim.cmd("split | terminal cd " .. vim.fn.expand("%:p:h") .. " && $SHELL")
 end)
 
+vim.keymap.set("n", "<leader>c.", function()
+  vim.cmd("vsp | terminal cd " .. vim.fn.expand("%:p:h") .. " && codex")
+end)
 
+vim.keymap.set("n", "<leader>c~", function()
+    vim.cmd("vsp | terminal codex")
+end)
--- lua/plugins/chatgpt.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set("n", "<leader>c.", function()
-  vim.cmd("vsp | terminal cd " .. vim.fn.expand("%:p:h") .. " && codex")
-end)
-
-vim.keymap.set("n", "<leader>c~", function()
-    vim.cmd("vsp | terminal codex")
-end)
