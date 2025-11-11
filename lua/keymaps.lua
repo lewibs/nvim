e0504ad0643488ba0fd5c8956aea5c343a0144a4
Your Name <you@example.com>
2025-11-11 00:52:17 -0500

    removed delete copy

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,9 +1,9 @@
-vim.keymap.set("n", "<CR>", "i", { noremap = true, silent = true })
-
+-- terminal
 vim.keymap.set("n", "<leader>t", function()
   vim.cmd("split | terminal cd " .. vim.fn.expand("%:p:h") .. " && $SHELL")
 end)
 
+-- chatgpt
 vim.keymap.set("n", "<leader>c.", function()
   vim.cmd("vsp | terminal cd " .. vim.fn.expand("%:p:h") .. " && codex")
 end)
@@ -11,3 +11,6 @@ end)
 vim.keymap.set("n", "<leader>c~", function()
     vim.cmd("vsp | terminal codex")
 end)
+
+-- delete without yanking
+vim.keymap.set({"n", "v"}, "d", '"_d')
